39caad7aa3a368ddfbf3ce4df794362053885a57
Lander2039 <lander2039@gmail.com>
2022-11-14 22:08:36 +0300

    Added lesson_17

--- app/src/main/java/com/example/kotlinlesson/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlesson/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.kotlinlesson
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.example.kotlinlesson.MainActivity2.Companion.startMainActivity2
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -12,15 +19,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor(getString(R.string.Black))))
+
         val button = findViewById<Button>(R.id.button1)
+        val textView = findViewById<TextView>(R.id.textView)
+        val btn = findViewById<Button>(R.id.btn)
 
         button.setOnClickListener {
             startActivity(
                 Intent(this, MainActivity2::class.java)
                 .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
                 .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
-// Маин активити 2 не будет добавлен в бекстек
-//                .addFlasgs(Intent.FLAG_ACTIVITY_NO_HISTORY))
         }
+
+        btn.setOnClickListener {
+            textView.text = getString(R.string.Hello_world)
+            startMainActivity2(this,
+            textView.text.toString() + getString(R.string.FromMainActivity))
+
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            android.R.id.home -> onBackPressed()
+            R.id.close_app -> finishAffinity()
+            R.id.goBack -> onBackPressed()
+        return super.onOptionsItemSelected(item)
+    }
     }
 }
--- app/src/main/java/com/example/kotlinlesson/MainActivity2.kt
+++ app/src/main/java/com/example/kotlinlesson/MainActivity2.kt
@@ -1,20 +1,55 @@
 package com.example.kotlinlesson
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "New title!"
+        supportActionBar?.setIcon(R.mipmap.ic_dumbbell_foreground)
+
         val button2 = findViewById<Button>(R.id.button3)
+
+        val textView2 = findViewById<TextView>(R.id.textView2)
+
+        val data: String = intent.getStringExtra(KEY) ?: getString(R.string.NoDate)
+
+        textView2.text = data
 
         button2.setOnClickListener {
             startActivity(
-                Intent(this, MainActivity3::class.java))
+                Intent(this, MainActivity3::class.java)
+            )
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home){
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+
+        private const val KEY = "1"
+
+        fun startMainActivity2(
+            context: Context,
+            string: String
+        ) {
+            val intent = Intent(context, MainActivity2::class.java)
+            intent.putExtra(KEY, string)
+            context.startActivity(intent)
         }
     }
 }
